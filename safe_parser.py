c88f1cb290e6d7d7ac421996aefd69aa8f407e47
DimaWoic <woicdv@gmail.com>
2021-04-20 19:20:33 +0300

    переписан метод get_products_safe, добавлен библиотека selenium для навигации по страницам

--- safe_parser.py
+++ safe_parser.py
@@ -1,25 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-
+from selenium import webdriver
+from os import getcwd
+import time
+from selenium.webdriver.support.ui import WebDriverWait
 
 class SafeParser():
 
     def __init__(self):
-        url_safe = 'http://surgut.snert.ru/category/seyfy'   #url на сейфы
-        url_wardr = 'http://surgut.snert.ru/category/metallicheskie-shkafy/'   #url на металлические шкафы
+        self.url_safe = 'http://surgut.snert.ru/category/seyfy'   #url на сейфы
+        self.url_wardr = 'http://surgut.snert.ru/category/metallicheskie-shkafy/'   #url на металлические шкафы
+        path_to_gecko = getcwd() + '/geckodriver'
+        options = webdriver.FirefoxOptions()
+        options.headless = False
+        self.driver = webdriver.Firefox(executable_path=path_to_gecko, options=options)
         user_agent = UserAgent()
         headers = {'User-Agent': user_agent.chrome}
-        page_get_safe = requests.get(url_safe, headers=headers)
-        page_get_wardr = requests.get(url_wardr, headers=headers)
+        page_get_safe = requests.get(self.url_safe, headers=headers)
         if page_get_safe.status_code == 200:
-            self.page_html_safe = BeautifulSoup(page_get_safe.text, 'html.parser')   #хранит страницу со списком товаров из категории сейфы
-            self.page_html_wardr = BeautifulSoup(page_get_wardr.text, 'html.parser') #хранит страницу со списком товаров из категории металлические шкафы
+            print("Подключение установлено")
         else:
             print('Ошибка соединения ' + str(page_get_safe.status_code))
 
+    def get_products_safe(self):
+        self.driver.get(self.url_safe)
+        elem_num_prod = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/div[2]/div[3]/div[1]/div[2]/a[4]')
+        elem_num_prod.click()
+        page_href = 'http://surgut.snert.ru/category/seyfy/?page='
+        #last_page = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/div[2]/div[3]/div[2]/ul/li[12]')
+        pagination = self.driver.find_element_by_class_name('prd-list-pagination')
+        print(pagination.text)
+        self.driver.close()
+
+
+        """
     def get_safe_product_list(self):
         product_list_ul = self.page_html_safe.find('ul', class_='product-list list')
         self.product_list = product_list_ul.findAll('li')
         return self.product_list
+        """
 
+parser = SafeParser()
+parser.get_products_safe()
