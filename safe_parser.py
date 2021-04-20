bfbd695db85a73a804c921b3ae5eae3c1c6b8034
DimaWoic <woicdv@gmail.com>
2021-04-20 21:02:48 +0300

    метод get_products_safe - запрашивает с первой страницы категории сейфы название продукта, старую и новую цену

--- safe_parser.py
+++ safe_parser.py
@@ -13,11 +13,11 @@ class SafeParser():
         self.url_wardr = 'http://surgut.snert.ru/category/metallicheskie-shkafy/'   #url на металлические шкафы
         path_to_gecko = getcwd() + '/geckodriver'
         options = webdriver.FirefoxOptions()
-        options.headless = False
+        options.headless = True
         self.driver = webdriver.Firefox(executable_path=path_to_gecko, options=options)
         user_agent = UserAgent()
-        headers = {'User-Agent': user_agent.chrome}
-        page_get_safe = requests.get(self.url_safe, headers=headers)
+        self.headers = {'User-Agent': user_agent.chrome}
+        page_get_safe = requests.get(self.url_safe, headers=self.headers)
         if page_get_safe.status_code == 200:
             print("Подключение установлено")
         else:
@@ -28,18 +28,18 @@ class SafeParser():
         elem_num_prod = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/div[2]/div[3]/div[1]/div[2]/a[4]')
         elem_num_prod.click()
         page_href = 'http://surgut.snert.ru/category/seyfy/?page='
-        #last_page = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/div[2]/div[3]/div[2]/ul/li[12]')
-        pagination = self.driver.find_element_by_class_name('prd-list-pagination')
-        print(pagination.text)
+        last_page = self.driver.find_element_by_xpath('/html/body/div[3]/div/div[1]/div[2]/div[3]/div[2]/ul/li[11]')
+        product_list = self.driver.find_elements_by_class_name('name')
+        for i in product_list:
+            product_href = i.find_element_by_tag_name('a').get_attribute('href')
+            r = requests.get(product_href, headers=self.headers).text
+            soup = BeautifulSoup(r, 'html.parser')
+            title = soup.find('h1', class_='product-name').text.replace('\n', '')
+            old_price = soup.find('span', class_='compare-at-price nowrap').text
+            new_price = soup.find('span', class_='bk_price price nowrap red').text
+            print(new_price)
         self.driver.close()
 
-
-        """
-    def get_safe_product_list(self):
-        product_list_ul = self.page_html_safe.find('ul', class_='product-list list')
-        self.product_list = product_list_ul.findAll('li')
-        return self.product_list
-        """
 
 parser = SafeParser()
 parser.get_products_safe()
